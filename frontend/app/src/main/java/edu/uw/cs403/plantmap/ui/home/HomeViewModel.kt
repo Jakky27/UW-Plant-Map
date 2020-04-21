fb42d1a9a9074047d607fde3e0b67934194143e0
Kouroche Bouchiat <kouroche.bouchiat@gmail.com>
2020-04-21 14:30:43 -0700

    Create Android Studio project for the frontend

---
+++ frontend/app/src/main/java/edu/uw/cs403/plantmap/ui/home/HomeViewModel.kt
@@ -0,0 +1,13 @@
+package edu.uw.cs403.plantmap.ui.home
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+
+class HomeViewModel : ViewModel() {
+
+    private val _text = MutableLiveData<String>().apply {
+        value = "This is home Fragment"
+    }
+    val text: LiveData<String> = _text
+}
