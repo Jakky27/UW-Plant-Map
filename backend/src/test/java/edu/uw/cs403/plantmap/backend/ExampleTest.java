c0e216a4905f0350c0269bd4f24186b81ccdb8db
Kouroche Bouchiat <kouroche.bouchiat@gmail.com>
2020-05-02 13:37:51 -0700

    Prepare backend/ project, add an example test so we can setup CI.

---
+++ backend/src/test/java/edu/uw/cs403/plantmap/backend/ExampleTest.java
@@ -0,0 +1,12 @@
+package edu.uw.cs403.plantmap.backend;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class ExampleTest {
+    @Test
+    public void testAddition() {
+        assertEquals(2, 1 + 1);
+    }
+}
