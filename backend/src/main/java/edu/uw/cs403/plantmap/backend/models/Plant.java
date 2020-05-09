9f29f2a54d130ed56612cc36c83546bac7404285
ice2meu <whuan@uw.edu>
2020-05-09 16:22:52 -0700

    Create basic models

--- backend/src/main/java/edu/uw/cs403/plantmap/backend/models/Plant.java
+++ backend/src/main/java/edu/uw/cs403/plantmap/backend/models/Plant.java
@@ -5,7 +5,7 @@ import lombok.Data;
 import java.util.UUID;
 
 @Data
-public class Plants {
+public class Plant {
     private UUID plant_id;
     private String name;
     private String description;
