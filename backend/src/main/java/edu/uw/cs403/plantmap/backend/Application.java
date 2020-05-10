fac2364d3ab73874446ef3148c5600be9c18affc
Kouroche Bouchiat <kouroche.bouchiat@gmail.com>
2020-05-10 15:58:32 -0700

    Fix Heroku deploy (port to bind is specified by the environment variable PORT)

--- backend/src/main/java/edu/uw/cs403/plantmap/backend/Application.java
+++ backend/src/main/java/edu/uw/cs403/plantmap/backend/Application.java
@@ -4,6 +4,17 @@ import static spark.Spark.*;
 
 public class Application {
     public static void main(String[] args) {
+        port(getHerokuAssignedPort());
+
         get("/", (req, res) -> "Welcome to the UW PlantMap API server.");
+    }
+
+    static int getHerokuAssignedPort() {
+        ProcessBuilder processBuilder = new ProcessBuilder();
+        if (processBuilder.environment().get("PORT") != null) {
+            return Integer.parseInt(processBuilder.environment().get("PORT"));
+        }
+
+        return 4567; //return default port if heroku-port isn't set (i.e. on localhost)
     }
 }
