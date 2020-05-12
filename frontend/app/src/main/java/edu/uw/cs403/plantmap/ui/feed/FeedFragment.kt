f85d2df5d98e7159b425b22483d5c3b9ad94c923
Jacky <jackymooc@gmail.com>
2020-05-12 13:39:03 -0700

    Setup for requests on frontend

--- frontend/app/src/main/java/edu/uw/cs403/plantmap/ui/feed/FeedFragment.kt
+++ frontend/app/src/main/java/edu/uw/cs403/plantmap/ui/feed/FeedFragment.kt
@@ -2,16 +2,21 @@ package edu.uw.cs403.plantmap.ui.feed
 
 import android.content.Context
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.ImageView
 import android.widget.TextView
 import androidx.fragment.app.Fragment
-import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProviders
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
+import com.android.volley.Request
+import com.android.volley.RequestQueue
+import com.android.volley.Response
+import com.android.volley.toolbox.JsonObjectRequest
+import com.android.volley.toolbox.Volley
 import com.squareup.picasso.Picasso
 import edu.uw.cs403.plantmap.R
 import java.util.*
@@ -20,6 +25,9 @@ class FeedFragment : Fragment() {
 
     private lateinit var feedViewModel: FeedViewModel
     private lateinit var postsView: RecyclerView
+    private lateinit var queue: RequestQueue
+
+    private lateinit var posts: ArrayList<Post>
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -31,23 +39,53 @@ class FeedFragment : Fragment() {
         val root = inflater.inflate(R.layout.fragment_feed, container, false)
 
         postsView = root.findViewById(R.id.postsView)
-        val posts: ArrayList<Post> = ArrayList()
+        posts = ArrayList()
         posts.add(Post("https://i.imgur.com/NoXtk.jpg", "This is the description of the flower", Date(10L)))
         posts.add(Post("https://i.imgur.com/NoXtk.jpg", "This is the description of the flower 2", Date(10L)))
         posts.add(Post("https://i.imgur.com/NoXtk.jpg", "This is the description of the flower 3", Date(10L)))
 
+        queue = Volley.newRequestQueue(this.context)
+        updatePostsList()
+
         postsView.layoutManager = LinearLayoutManager(this.context)
         postsView.adapter = PostAdapter(posts, this.context!!)
 
+
         return root
+    }
+
+    private fun updatePostsList() {
+        // TODO communicate with server here
+
+        val url = "https://plantmap.herokuapp.com"
+
+        val jsonObjectRequest = JsonObjectRequest(Request.Method.GET, url, null,
+            Response.Listener { response ->
+                // Update flowers here
+                val temp = "Response: %s".format(response.toString())
+                //val aJsonString: String = jObject.getString("STRINGNAME")
+
+                Log.d("DEBUG", temp)
+
+            },
+            Response.ErrorListener { error ->
+                // TODO: Handle error
+                Log.d("DEBUG", "Handle error")
+                Log.d("DEBUG", error.message)
+
+            }
+        )
+
+        val image_url = "/v1/image?id="
+
+        // TODO: with all the submissions, now request for images
+
+        queue.add((jsonObjectRequest)) // automatically requests
+
     }
 }
 
-private fun updatePostsList() {
-    // TODO communicate with server here
 
-    // TODO update flowers
-}
 
 private data class Post(val photo: String, val description: String, val date: Date)
 
