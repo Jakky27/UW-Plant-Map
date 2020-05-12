a784b625b80d51d28435213fd65372d51ac151ce
ice2meu <whuan@uw.edu>
2020-05-12 13:15:55 -0700

    merge Main with Application

--- backend/src/main/java/edu/uw/cs403/plantmap/backend/Application.java
+++ backend/src/main/java/edu/uw/cs403/plantmap/backend/Application.java
@@ -1,5 +1,11 @@
 package edu.uw.cs403.plantmap.backend;
 
+import edu.uw.cs403.plantmap.backend.controllers.PlantController;
+import edu.uw.cs403.plantmap.backend.models.PlantServerImp;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+
 import static spark.Spark.*;
 
 public class Application {
@@ -7,6 +13,27 @@ public class Application {
         port(getHerokuAssignedPort());
 
         get("/", (req, res) -> "Welcome to the UW PlantMap API server.");
+
+
+        try {
+            // start database connection, comment
+            Connection conn = startDbConnect(); // comment me for testing
+
+            // TODO: add controllers
+            PlantController plantCtr = new PlantController(new PlantServerImp(conn));
+
+            post("/v1/plant", (req, res) -> plantCtr.addPlant(req, res));
+            get("/v1/plant", (req, res) -> plantCtr.getPlant(req, res));
+
+
+            // Test without DB
+//        PlantController plantCtrTest = new PlantController(new PlantServerTest());
+//        post("/v1/plant", (req, res) -> plantCtrTest.addPlant(req, res));
+
+
+        } catch (Exception e){
+            e.printStackTrace();
+        }
     }
 
     static int getHerokuAssignedPort() {
@@ -16,5 +43,27 @@ public class Application {
         }
 
         return 4567; //return default port if heroku-port isn't set (i.e. on localhost)
+    }
+
+    public static Connection startDbConnect() {
+        // TODO: Add Azure DB information
+        String hostName = "your_server.database.windows.net"; // update me
+        String dbName = "your_database"; // update me
+        String user = "your_username"; // update me
+        String password = "your_password"; // update me
+        String url = String.format("jdbc:sqlserver://%s:1433;database=%s;user=%s;password=%s;encrypt=true;"
+                + "hostNameInCertificate=*.database.windows.net;loginTimeout=30;", hostName, dbName, user, password);
+        Connection connection = null;
+
+        try {
+            connection = DriverManager.getConnection(url);
+            String schema = connection.getSchema();
+            System.out.println("Successful connection - Schema: " + schema);
+        }
+        catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        return connection;
     }
 }
