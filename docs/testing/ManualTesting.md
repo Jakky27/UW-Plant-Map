4d3eaba7bceef566c86ce9e2eaa2e329fda120b2
Daniel Merken <dcm58@cs.washington.edu>
2020-05-19 18:00:16 -0700

    Added tests for the front end
    Added unit tests for the frontend's client. Additionally, added manual test descriptions for the overall app.

---
+++ docs/testing/ManualTesting.md
@@ -0,0 +1,15 @@
+# Manual Tests
+Register a Plant:
+1. Ensure location and camera permissions are enabled and that the app has a stable internet connection
+2. Navigate to the “map” section, verify that some submissions appear on the map
+3. Click the floating action button in the top right corner of the map with a “+” sign on it to begin registering a plant
+4. Enter a name, description, and take a photo.
+5. Click on "register"
+6. Verify the new submission appears on the map
+
+Check Map:
+1. Ensure location and camera permissions are enabled and the app has a stable internet connection
+2. Navigate to the "map" section, verify that some submissions appear on the map
+3. Verify selecting a marker on the map reveals information about the submission
+4. Verify that your location appears on the map
+5. Verify that you cannot pan the map's camera outside of a set radius around UW
---
+++ frontend/app/src/test/java/edu/uw/cs403/plantmap/clients/BackendClientTest.kt
@@ -0,0 +1,110 @@
+package edu.uw.cs403.plantmap.clients
+
+import com.android.volley.Request
+import com.android.volley.Response
+import com.android.volley.toolbox.StringRequest
+import com.fasterxml.jackson.databind.ObjectMapper
+import com.nhaarman.mockitokotlin2.KArgumentCaptor
+import com.nhaarman.mockitokotlin2.argumentCaptor
+import com.nhaarman.mockitokotlin2.mock
+import edu.uw.cs403.plantmap.models.Submission
+import org.junit.Before
+import org.junit.Test
+import org.mockito.Mockito.verify
+
+class BackendClientTest {
+    // API paths
+    private val API_BASE_URL = "https://plantmap.herokuapp.com/v1/"
+    private val API_SUBMISSION_PATH = API_BASE_URL + "submission"
+    private val API_PLANT_PATH = API_BASE_URL + "plant"
+
+    private val MAPPER = ObjectMapper()
+
+    lateinit var backendClient: BackendClient
+    lateinit var mockRequestQueueSingleton: RequestQueueSingleton
+    lateinit var mockErrorListener: Response.ErrorListener
+    lateinit var requestCaptor: KArgumentCaptor<StringRequest>
+
+    @Before
+    fun init() {
+        mockErrorListener = mock()
+
+        mockRequestQueueSingleton = mock()
+        backendClient = BackendClient(mockRequestQueueSingleton)
+
+        requestCaptor = argumentCaptor()
+    }
+
+    @Test
+    fun testGetSubmissions() {
+        val listener = mock<Response.Listener<List<Submission>>>()
+
+        backendClient.getSubmissions(listener, mockErrorListener)
+
+        verify(mockRequestQueueSingleton).addToRequestQueue(requestCaptor.capture())
+
+        val r = requestCaptor.firstValue
+
+        assert(r.method == Request.Method.GET)
+        assert(r.url == API_SUBMISSION_PATH)
+        assert(r.errorListener == mockErrorListener)
+    }
+
+    @Test
+    fun testPostPlant() {
+        val name = "testName"
+        val desc = "testDescription"
+        val listener = mock<Response.Listener<Int>>()
+
+        backendClient.postPlant(name, desc, listener, mockErrorListener)
+
+        verify(mockRequestQueueSingleton).addToRequestQueue(requestCaptor.capture())
+
+        val r = requestCaptor.firstValue
+
+        assert(r.method == Request.Method.POST)
+        assert(r.url == API_PLANT_PATH)
+        assert(r.errorListener == mockErrorListener)
+
+        // Check body is correct
+
+        val body = MAPPER.readTree(r.body)
+
+        assert(body.get("name").asText() == name)
+        assert(body.get("description").asText() == desc)
+        assert(body.size() == 2)
+    }
+
+    @Test
+    fun testPostSubmission() {
+        val plantId = 1
+        val latitude = 2f
+        val longitude = 3f
+        val postedOn = 5L
+        val postedBy = "Test User"
+
+        val listener = mock<Response.Listener<Int>>()
+
+        backendClient.postSubmission(plantId, latitude, longitude, postedOn, postedBy, listener,
+            mockErrorListener)
+
+        verify(mockRequestQueueSingleton).addToRequestQueue(requestCaptor.capture())
+
+        val r = requestCaptor.firstValue
+
+        assert(r.method == Request.Method.POST)
+        assert(r.url == API_SUBMISSION_PATH)
+        assert(r.errorListener == mockErrorListener)
+
+        // Check body is correct
+        val body = MAPPER.readTree(r.body)
+
+        assert(body.get("plant_id").asInt() == plantId)
+        assert(body.get("latitude").asDouble().toFloat() == latitude)
+        assert(body.get("longitude").asDouble().toFloat() == longitude)
+        assert(body.get("posted_on").asLong() == postedOn)
+        assert(body.get("posted_by").asText() == postedBy)
+
+        assert(body.size() == 5)
+    }
+}
