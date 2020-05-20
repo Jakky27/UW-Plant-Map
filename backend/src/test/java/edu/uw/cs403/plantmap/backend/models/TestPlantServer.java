fd4da85dcdbd0104322cb57934c7ed691109a69e
bparadee <bparadee@uw.edu>
2020-05-19 20:56:02 -0700

    Created basic JUnit tests for server and database functionality

--- backend/src/test/java/edu/uw/cs403/plantmap/backend/models/TestPlantServer.java
+++ backend/src/test/java/edu/uw/cs403/plantmap/backend/models/TestPlantServer.java
@@ -1,12 +1,43 @@
 package edu.uw.cs403.plantmap.backend.models;
 
+import edu.uw.cs403.plantmap.backend.Application;
+import edu.uw.cs403.plantmap.backend.SQLConnectionPool;
+import org.junit.Before;
 import org.junit.Test;
+
+import static org.junit.Assert.*;
 
 public class TestPlantServer {
 
+    private PlantServerImp psi;
+
+    @Before
+    public void setUp() {
+        SQLConnectionPool pool = Application.createConnectionPool();
+        psi = new PlantServerImp(pool);
+    }
+
     @Test
     public void testRegisterPlant(){
+        try {
+            // register the plant initially
+            String name = "Rare Testing Plant";
+            int id = psi.registerPlant(name, "This is a testing plant description");
+            assertTrue(id > 0);
 
+            Plant idp = psi.getPlantById(id);
+            assertNotNull(idp);
+
+            Plant np = psi.getPlantByName(name);
+            assertNotNull(np);
+            assertEquals(idp.hashCode(), np.hashCode());
+
+            int did = psi.deletePlant(id);
+            assertEquals(did, id);
+
+        } catch(Exception E) {
+            fail();
+        }
     }
 
 }
--- backend/src/test/java/edu/uw/cs403/plantmap/backend/models/TestSubmissionServer.java
+++ backend/src/test/java/edu/uw/cs403/plantmap/backend/models/TestSubmissionServer.java
@@ -1,6 +1,48 @@
 package edu.uw.cs403.plantmap.backend.models;
 
+import edu.uw.cs403.plantmap.backend.Application;
+import edu.uw.cs403.plantmap.backend.SQLConnectionPool;
+import org.junit.Before;
+import org.junit.Test;
+
+import java.util.Date;
+
+import static org.junit.Assert.*;
+
 public class TestSubmissionServer {
 
+    private SubmissionServerImp ssi;
+    private PlantServerImp psi;
+
+    @Before
+    public void setUp() {
+        SQLConnectionPool pool = Application.createConnectionPool();
+        ssi = new SubmissionServerImp(pool);
+        psi = new PlantServerImp(pool);
+
+    }
+
+    @Test
+    public void testPostCycle(){
+        try {
+            // register the submission initially
+            String name = "Rare Testing Plant";
+            Date d = new Date();
+            long input_date = d.getTime();
+            int pid = psi.registerPlant("TestPlant", "TestPlant Description");
+
+            int id = ssi.createSubmission("Test User",input_date, pid, (float)-122.335, (float)47.608);
+            assertTrue(id > 0);
+
+            Submission ids = ssi.getSubmission(id);
+            assertNotNull(ids);
+
+            int did = ssi.deleteSubmission(id);
+            assertEquals(did, id);
+
+        } catch(Exception E) {
+            fail();
+        }
+    }
 
 }
