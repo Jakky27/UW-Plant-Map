640f9358507a8d2149cc22fdc269a378fc85a37c
Kouroche Bouchiat <kouroche.bouchiat@gmail.com>
2020-05-23 13:18:39 -0700

    Fix exception throwing in the image controller

--- backend/src/main/java/edu/uw/cs403/plantmap/backend/controllers/ImageController.java
+++ backend/src/main/java/edu/uw/cs403/plantmap/backend/controllers/ImageController.java
@@ -13,28 +13,19 @@ public class ImageController {
         this.server = server;
     }
 
-    public Object uploadImg(Request request, Response response){
-        try{
-            // parse submission id from URL
-            int sub_id = Integer.parseInt(request.params("id"));
-            server.updateSubmission(sub_id, request.bodyAsBytes());
-            response.type("text/plain");
-            return sub_id;
-        } catch (Exception e){
-            throw new RuntimeException("Failed to store image");
-        }
-
+    public Object uploadImg(Request request, Response response) throws Exception {
+        // parse submission id from URL
+        int sub_id = Integer.parseInt(request.params("id"));
+        server.updateSubmission(sub_id, request.bodyAsBytes());
+        response.type("text/plain");
+        return sub_id;
     }
 
-    public Object getImg(Request request, Response response){
-        try {
-            int sub_id = Integer.parseInt(request.params("id"));
-            Submission sub = server.getSubmission(sub_id);
-            response.type("application/octet-stream");
-            return sub.getImg();
-        } catch (Exception e) {
-            throw new RuntimeException("Failed to get image");
-        }
+    public Object getImg(Request request, Response response) throws Exception {
+        int sub_id = Integer.parseInt(request.params("id"));
+        Submission sub = server.getSubmission(sub_id);
+        response.type("application/octet-stream");
+        return sub.getImg();
     }
 
 }
