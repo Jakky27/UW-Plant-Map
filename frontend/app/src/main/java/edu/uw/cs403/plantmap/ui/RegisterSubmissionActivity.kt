0a43569b0ba5e96a0b76008f73af29d5b1ebe875
Daniel Merken <dcm58@cs.washington.edu>
2020-05-29 14:51:41 -0700

    Updated registering images for high quality images

--- frontend/app/src/main/java/edu/uw/cs403/plantmap/ui/RegisterSubmissionActivity.kt
+++ frontend/app/src/main/java/edu/uw/cs403/plantmap/ui/RegisterSubmissionActivity.kt
@@ -4,21 +4,25 @@ import android.Manifest
 import android.content.Intent
 import android.content.pm.PackageManager
 import android.graphics.Bitmap
+import android.graphics.BitmapFactory
 import android.location.Location
+import android.net.Uri
 import android.os.Bundle
+import android.os.Environment
 import android.provider.MediaStore
 import android.widget.*
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.content.ContextCompat
+import androidx.core.content.FileProvider
 import com.android.volley.Response
-import com.android.volley.toolbox.StringRequest
 import com.google.android.gms.location.FusedLocationProviderClient
 import com.google.android.gms.location.LocationServices
 import edu.uw.cs403.plantmap.R
 import edu.uw.cs403.plantmap.clients.BackendClient
 import edu.uw.cs403.plantmap.clients.RequestQueueSingleton
 import edu.uw.cs403.plantmap.clients.UWPlantMapClient
-import org.json.JSONObject
+import java.io.File
+import java.io.IOException
 
 /**
  * Activity representing the flow for registering a new submission
@@ -29,6 +33,7 @@ class RegisterSubmissionActivity : AppCompatActivity() {
     val NO_LOCATION_TEXT = "Please enable location services"
     val NO_CAMERA_TEXT = "Please enable camera"
     val NETWORK_ERROR_TEXT = "Could not upload submission: "
+    val FILE_SAVE_TEXT = "Could not save image file"
     val DURATION = Toast.LENGTH_SHORT
 
     private lateinit var client: UWPlantMapClient
@@ -41,7 +46,8 @@ class RegisterSubmissionActivity : AppCompatActivity() {
     private lateinit var descriptionEditText: EditText
     private lateinit var nameEditText: EditText
 
-    private lateinit var image : Bitmap
+    private lateinit var currentPhotoPath: String
+    private lateinit var image: Bitmap
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -122,7 +128,24 @@ class RegisterSubmissionActivity : AppCompatActivity() {
     private fun dispatchTakePictureIntent() {
         Intent(MediaStore.ACTION_IMAGE_CAPTURE).also { takePictureIntent ->
             takePictureIntent.resolveActivity(packageManager)?.also {
-                startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAPTURE)
+                // Create the File where the photo should go
+                val photoFile: File? = try {
+                    createImageFile()
+                } catch (ex: IOException) {
+                    // Error occurred while creating the File
+                    Toast.makeText(this, FILE_SAVE_TEXT, DURATION).show()
+                    null
+                }
+                // Continue only if the File was successfully created
+                photoFile?.also {
+                    val photoURI: Uri = FileProvider.getUriForFile(
+                        this,
+                        "edu.uw.cs403.plantmap.fileprovider",
+                        it
+                    )
+                    takePictureIntent.putExtra(MediaStore.EXTRA_OUTPUT, photoURI)
+                    startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAPTURE)
+                }
             }
         }
     }
@@ -132,10 +155,21 @@ class RegisterSubmissionActivity : AppCompatActivity() {
      */
     override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
         if (requestCode == REQUEST_IMAGE_CAPTURE && resultCode == RESULT_OK) {
-            val imageBitmap = data!!.extras!!.get("data") as Bitmap
-            // Update photo imageview and store bitmap
-            plantImageView.setImageBitmap(imageBitmap)
-            image = imageBitmap
+            image = BitmapFactory.decodeFile(currentPhotoPath)
+            plantImageView.setImageBitmap(image)
+        }
+    }
+
+    private fun createImageFile(): File {
+        // Create an image file name
+        val storageDir: File = getExternalFilesDir(Environment.DIRECTORY_PICTURES)!!
+        return File.createTempFile(
+            "JPEG_submission_", /* prefix */
+            ".jpg", /* suffix */
+            storageDir /* directory */
+        ).apply {
+            // Save a file: path for use with ACTION_VIEW intents
+            currentPhotoPath = absolutePath
         }
     }
 
