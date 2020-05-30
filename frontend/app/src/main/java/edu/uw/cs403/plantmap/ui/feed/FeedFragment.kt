820c4a943da12e9bf4db1b5dc6c88562b514dcd9
Daniel Merken <dcm58@cs.washington.edu>
2020-05-30 14:10:12 -0700

    Updated feed to use client and link to view submissions

--- frontend/app/src/main/java/edu/uw/cs403/plantmap/ui/feed/FeedFragment.kt
+++ frontend/app/src/main/java/edu/uw/cs403/plantmap/ui/feed/FeedFragment.kt
@@ -1,8 +1,7 @@
 package edu.uw.cs403.plantmap.ui.feed
 
 import android.content.Context
-import android.database.DataSetObserver
-import android.graphics.Bitmap
+import android.content.Intent
 import android.os.Bundle
 import android.util.Log
 import android.view.LayoutInflater
@@ -14,32 +13,25 @@ import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProviders
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
-import com.android.volley.Request
-import com.android.volley.RequestQueue
 import com.android.volley.Response
-import com.android.volley.toolbox.*
-import com.squareup.picasso.Callback
 import com.squareup.picasso.Picasso
 import edu.uw.cs403.plantmap.R
-import org.json.JSONArray
-import org.json.JSONObject
-import java.util.*
-import kotlin.math.log
 import edu.uw.cs403.plantmap.clients.BackendClient
 import edu.uw.cs403.plantmap.clients.RequestQueueSingleton
 import edu.uw.cs403.plantmap.clients.UWPlantMapClient
+import edu.uw.cs403.plantmap.models.Plant
+import edu.uw.cs403.plantmap.models.Submission
+import edu.uw.cs403.plantmap.ui.ViewSubmissionActivity
+import java.util.*
 
 class FeedFragment : Fragment() {
 
     private lateinit var feedViewModel: FeedViewModel
     private lateinit var postsView: RecyclerView
-    private lateinit var queue: RequestQueue
 
     private lateinit var posts: ArrayList<Post>
 
     private lateinit var client: UWPlantMapClient
-
-    private var postCount = 0
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -52,12 +44,10 @@ class FeedFragment : Fragment() {
 
         postsView = root.findViewById(R.id.postsView)
         posts = ArrayList()
-        queue = Volley.newRequestQueue(this.context)
-        updatePostsList()
-
         client = BackendClient.getInstance(
             RequestQueueSingleton.getInstance(this.context!!.applicationContext)
         )
+        updatePostsList()
 
         postsView.layoutManager = LinearLayoutManager(this.context)
         postsView.adapter = PostAdapter(posts, this.context!!)
@@ -72,65 +62,46 @@ class FeedFragment : Fragment() {
      *
      */
     private fun updatePostsList() {
-        val url = "https://plantmap.herokuapp.com/v1/submission"
-        val descriptionURL = "https://plantmap.herokuapp.com/v1/plant/"
-        val imgURL = "https://plantmap.herokuapp.com/v1/image/"
+        client.getSubmissions(
+            Response.Listener<List<Submission>> { submissions ->
+                for (submission in submissions) {
+                    val plantId = submission.plant_id!!
+                    val subId = submission.post_id!!
+                    Log.d("DEBUG", "REQUEST");
 
-        postCount = 0
+                    client.getPlant(plantId, Response.Listener<Plant> {plant ->
 
-        val jsonObjectRequest = JsonArrayRequest(Request.Method.GET, url, null,
-            Response.Listener { response ->
+                        posts.add(Post(submission, plant))
 
-                Log.d("DEBUG", response.length().toString())
-
-                for(x in 0 until response.length()) {
-                    val res = response[x] as JSONObject
-
-                    val subID = res.getString("post_id")
-                    val date = res.getLong("post_date")
-
-                    val length = response.length()
-
-                    val descriptionObjectRequest = JsonObjectRequest(Request.Method.GET, descriptionURL + subID, null,
-                        Response.Listener { response ->
-
-                            val plantName = response.getString("name")
-                            val plantDescription = response.getString("description")
-
-                            posts.add(Post(plantName, imgURL + subID, plantDescription, Date(date)))
-                            posts.sortByDescending { it.date }
-
-                            postCount++
-
-                            if (postCount == length) {
-                                postsView.adapter!!.notifyDataSetChanged()
-                            }
-
-
-                        }, Response.ErrorListener { error ->
-                            Log.d("DEBUG", "Description Handle error")
-                            //Log.d("DEBUG", error.message)
+                        if (posts.size == submissions.size) {
+                            posts.sortBy{ it.submission.post_date }
+                            postsView.adapter!!.notifyDataSetChanged()
                         }
-                    )
-                    queue.add(descriptionObjectRequest)
+                    },
+                    Response.ErrorListener {
+                        Log.d("DEBUG", "Description Handle error")
+                    })
                 }
             },
-            Response.ErrorListener { error ->
+            Response.ErrorListener {
                 Log.d("DEBUG", "Submission Handle error")
-                //Log.d("DEBUG", error.message)
             }
         )
-        queue.add(jsonObjectRequest) // automatically requests
     }
 }
 
 
 
-public data class Post(val plantName: String, val photo: String, val description: String, val date: Date)
+data class Post(val submission: Submission, val plant: Plant)
 
 private class PostAdapter(val posts: ArrayList<Post>, val context: Context) : RecyclerView.Adapter<PostAdapter.ViewHolder>() {
 
+    private lateinit var client: UWPlantMapClient
+
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        client = BackendClient.getInstance(
+            RequestQueueSingleton.getInstance(this.context.applicationContext)
+        )
         return ViewHolder(LayoutInflater.from(parent.context).inflate(R.layout.live_feed_post, parent, false))
     }
 
@@ -138,16 +109,21 @@ private class PostAdapter(val posts: ArrayList<Post>, val context: Context) : Re
 
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
 
-        holder.plantName.text = posts[position].plantName
+        holder.plantName.text = posts[position].plant.name
 
-        holder.description.text = posts[position].description
+        holder.description.text = posts[position].plant.description
 
         Picasso.get()
-            .load(posts[position].photo)
+            .load("https://plantmap.herokuapp.com/v1/image/" + posts[position].submission.post_id)
             .into(holder.postPhoto)
 
+        holder.postDate.text = String.format("%tc", Date(posts[position].submission.post_date!!))
 
-        holder.postDate.text = String.format("%tc", posts[position].date);
+        holder.itemView.setOnClickListener { _ ->
+            val intent = Intent(context, ViewSubmissionActivity::class.java)
+            intent.putExtra("submissionId", posts[position].submission.post_id)
+            context.startActivity(intent)
+        }
     }
 
     class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
